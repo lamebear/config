6dda60d6e466b5461f01a02a3c900ff0438ededd
Jacob Lambert <jacob@jacoblambert.com>
2019-09-07 12:55:21 -0600

    set git user info:

--- init.sh
+++ init.sh
@@ -6,6 +6,11 @@ ln -s $dir/profile/.bash_profile $HOME/.bash_profile
 ln -s $dir/profile/.bash_aliases $HOME/.bash_aliases
 ln -s $dir/profile/.bash_functions $HOME/.bash_functions
 
+read -p "Git User Email:" userEmail
+
+git config --global user.name "Jacob Lambert"
+git config --global user.email $userEmail
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
